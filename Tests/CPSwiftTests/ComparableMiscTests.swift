4b4c81f961ed7526b6171d30ed95af297210be74
judau <jusk1nsnt9gi>
2022-12-13 13:48:04 +0900

    漏れていた観点を追加

--- Tests/CPSwiftTests/ComparableMiscTests.swift
+++ Tests/CPSwiftTests/ComparableMiscTests.swift
@@ -4,34 +4,50 @@ import XCTest
 final class ComparableMiscTests: XCTestCase {
     func testInferiorMergeIntValues() {
         for _ in 0..<100 {
-            var a = Int.random(in: 50..<100)
+            let a = Int.random(in: 50..<100)
             let b = Int.random(in: 0..<a)
-            XCTAssertTrue(a.inferiorMerge(using: b))
-            XCTAssertEqual(a, b)
+            var (p, q) = (a, b)
+            XCTAssertFalse(q.inferiorMerge(using: a))
+            XCTAssertEqual(q, b)
+            XCTAssertTrue(p.inferiorMerge(using: b))
+            XCTAssertEqual(p, b)
+            XCTAssertEqual(p, q)
         }
     }
     func testSuperiorMergeIntValues() {
         for _ in 0..<100 {
-            var a = Int.random(in: 0..<50)
+            let a = Int.random(in: 0..<50)
             let b = Int.random(in: (a + 1)..<100)
-            XCTAssertTrue(a.superiorMerge(using: b))
-            XCTAssertEqual(a, b)
+            var (p, q) = (a, b)
+            XCTAssertFalse(q.superiorMerge(using: a))
+            XCTAssertEqual(q, b)
+            XCTAssertTrue(p.superiorMerge(using: b))
+            XCTAssertEqual(p, b)
+            XCTAssertEqual(p, q)
         }
     }
     func testInferiorMergeDoubleValues() {
         for _ in 0..<100 {
-            var a = Double.random(in: 50.0..<100.0)
+            let a = Double.random(in: 50.0..<100.0)
             let b = Double.random(in: 0.0..<a)
-            XCTAssertTrue(a.inferiorMerge(using: b))
-            XCTAssertEqual(a, b)
+            var (p, q) = (a, b)
+            XCTAssertFalse(q.inferiorMerge(using: a))
+            XCTAssertEqual(q, b)
+            XCTAssertTrue(p.inferiorMerge(using: b))
+            XCTAssertEqual(p, b)
+            XCTAssertEqual(p, q)
         }
     }
     func testSuperiorMergeDoubleValues() {
         for _ in 0..<100 {
-            var a = Double.random(in: 0.0..<50.0)
+            let a = Double.random(in: 0.0..<50.0)
             let b = Double.random(in: (a + 1.0)..<100.0)
-            XCTAssertTrue(a.superiorMerge(using: b))
-            XCTAssertEqual(a, b)
+            var (p, q) = (a, b)
+            XCTAssertFalse(q.superiorMerge(using: a))
+            XCTAssertEqual(q, b)
+            XCTAssertTrue(p.superiorMerge(using: b))
+            XCTAssertEqual(p, b)
+            XCTAssertEqual(p, q)
         }
     }
 }
