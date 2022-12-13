afab92ef512b2d4742e9d1a00a9f6539d9230131
judau <jusk1nsnt9gi>
2022-12-13 19:51:41 +0900

    なんかnilが怖くなってきたので

--- Sources/CPSwift/Extensions/StringMisc.swift
+++ Sources/CPSwift/Extensions/StringMisc.swift
@@ -6,13 +6,40 @@ import Foundation
 
 
 extension Character {
-    func asciiIntValue() -> Int? { Int(asciiValue!) }
-    func isUppercaseAlphabet() -> Bool { (65...90) ~= asciiIntValue()! }
-    func isLowercaseAlphabet() -> Bool { (97...122) ~= asciiIntValue()! }
+    func asciiIntValue() -> Int? {
+        guard let ascv = asciiValue else { return nil }
+        return Int(ascv)
+    }
+    func isUppercaseAlphabet() -> Bool {
+        guard let aiv = asciiIntValue() else { return false }
+        return (65...90) ~= aiv
+    }
+    func isLowercaseAlphabet() -> Bool {
+        guard let aiv = asciiIntValue() else { return false }
+        return (97...122) ~= aiv
+    }
     func isAlphabet() -> Bool { isUppercaseAlphabet() || isLowercaseAlphabet() }
 }
 
 extension String {
     func values() -> [Character] { Array(self) }
-    func asciiIntValues() -> [Int]? { values().map { $0.asciiIntValue()! } }
+    func asciiIntValues() -> [Int]? {
+        var res = [Int]()
+        for v in values() {
+            guard let vav = v.asciiIntValue() else { return nil }
+            res.append(vav)
+        }
+        return res
+    }
+    func bitArray() -> [Bool]? {
+        var res = [Bool]()
+        for c in values() {
+            switch c {
+            case "1": res.append(true)
+            case "0": res.append(false)
+            default: return nil
+            }
+        }
+        return res
+    }
 }
--- Tests/CPSwiftTests/StringMiscTests.swift
+++ Tests/CPSwiftTests/StringMiscTests.swift
@@ -7,4 +7,11 @@ final class StringMiscTests: XCTestCase {
         XCTAssertEqual(str.values(), ["1", "2", "3", "4", "a", "s", "d", "f"])
         XCTAssertEqual(str.asciiIntValues()!, [49, 50, 51, 52, 97, 115, 100, 102])
     }
+    func testAsciiIntValues() {
+        let str = "1234asdf"
+        let asciiIntValues = str.asciiIntValues()
+        XCTAssertNotNil(asciiIntValues)
+        XCTAssertEqual(asciiIntValues!, [49, 50, 51, 52, 97, 115, 100, 102])
+
+    }
 }
