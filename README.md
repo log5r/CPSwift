585d03ffeb999b988f814438ddcf88100f729f83
log5 <40707031+log5r@users.noreply.github.com>
2022-12-13 12:20:56 +0900

    いろいろ追加

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CPSwift
+
 Swift snippets for Competitive programming
+
+## Note
+
+最初は1つのファイルに書いて、膨張したら必要に応じてファイルを分ける
---
+++ Snippets/ComparableMisc/ComparableMisc.swift
@@ -0,0 +1,21 @@
+extension Comparable {
+
+    @discardableResult
+    mutating func superiorMerge(using value: Self) -> Bool {
+        if self < value {
+            self = value
+            return true
+        }
+        return false
+    }
+
+    @discardableResult
+    mutating func inferiorMerge(using value: Self) -> Bool {
+        if self > value {
+            self = value
+            return true
+        }
+        return false
+    }
+    
+}
